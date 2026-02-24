d0564fa0b279ed116c82f17f4ef24ae02d9ff8ac
Lucas Marion <lucasmarion.dev@gmail.com>
2026-02-24 19:48:12 -0300

    Aula 8

--- index.php
+++ index.php
@@ -8,4 +8,25 @@ $stmt = $pdo->query("SELECT * FROM product");
 
 $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-print_r($products);
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Products</title>
+        <meta charset="UTF-8">
+    </head>
+
+    <body>
+        
+        <h1>Products</h1>
+
+        <?php foreach ($products as $product): ?>
+            <h2><?= htmlspecialchars($product["name"]) ?></h2>
+            <p><?= htmlspecialchars($product["description"]) ?></p>
+        <?php endforeach ?>
+
+    </body>
+
+</html>
