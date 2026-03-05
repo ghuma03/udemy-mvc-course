f0bf3a7080d2289964c739a06eff89a1c949deb5
Lucas Marion <lucasmarion.dev@gmail.com>
2026-03-04 22:19:15 -0300

    Aula 18

--- index.php
+++ index.php
@@ -3,31 +3,7 @@
 $controller = $_GET["controller"];
 $action = $_GET["action"];
 
-switch ($controller) {
+require "src/controllers/$controller.php";
 
-    case "products":
-        require "src/controllers/products.php";
-        $controller = new Products;
-        break;
-
-    case "home":
-        require "src/controllers/home.php";
-        $controller = new Home;
-        break;
-    
-    default:
-        echo "Controller not found!";
-        die();
-}
-
-switch ($action) {
-    case "index":
-        $controller->index();
-        break;
-    case "show":
-        $controller->show();
-        break;
-    default:
-        echo "Action not found!";
-        die();
-}
+$controller_object = new $controller;
+    $controller_object->$action();
