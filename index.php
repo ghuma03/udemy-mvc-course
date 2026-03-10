9996f1affa46f76c48bcf1bafccba4928bf18f68
Lucas Marion <lucasmarion.dev@gmail.com>
2026-03-10 15:14:41 -0300

    Aula 59

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
 $router = new Framework\Router;
     $router->add("/{controller}/{action}");
-    $router->add("/{controller}/{id}/{action}");
+    $router->add("/{controller}/{id:\d+}/{action}");
     $router->add("/home/index"          , array("controller" => "home"      , "action" => "index"));
     $router->add("/products"            , array("controller" => "products"  , "action" => "index"));
     $router->add("/"                    , array("controller" => "home"      , "action" => "index"));
