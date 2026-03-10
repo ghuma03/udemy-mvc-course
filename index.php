dd280b396af81235649868d4220f3a9631d6458a
Lucas Marion <lucasmarion.dev@gmail.com>
2026-03-10 11:54:23 -0300

    Aula 26

--- index.php
+++ index.php
@@ -6,10 +6,14 @@ $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 require "src/router.php";
 
 $router = new Router();
-    $router->add("/home/index", array("controller" => "home", "action" => "index"));
-    $router->add("/products", array("controller" => "products", "action" => "index"));
-    $router->add("/", array("controller" => "home", "action" => "index"));
-    
+    $router->add("/home/index"  , array("controller" => "home"      , "action" => "index"));
+    $router->add("/products"    , array("controller" => "products"  , "action" => "index"));
+    $router->add("/"            , array("controller" => "home"      , "action" => "index"));
+
+$params = $router->match($path);
+var_dump($params);
+die();
+
 $segments = explode("/", $path);
 
 $controller = $segments[1];
