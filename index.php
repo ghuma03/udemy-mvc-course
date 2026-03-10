434494171c2fd61c2d8e53113b6078483fae6555
Lucas Marion <lucasmarion.dev@gmail.com>
2026-03-10 15:19:19 -0300

    Aula 62

--- index.php
+++ index.php
@@ -7,12 +7,12 @@ spl_autoload_register(function($class_name) {
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
 $router = new Framework\Router;
-    $router->add("/{controller}/{action}");
     $router->add("/product/{slug:[\w-]+}", array("controller" => "products", "action" => "show"));
     $router->add("/{controller}/{id:\d+}/{action}");
-    $router->add("/home/index"          , array("controller" => "home"      , "action" => "index"));
-    $router->add("/products"            , array("controller" => "products"  , "action" => "index"));
-    $router->add("/"                    , array("controller" => "home"      , "action" => "index"));
+    $router->add("/home/index", array("controller" => "home"      , "action" => "index"));
+    $router->add("/products", array("controller" => "products"  , "action" => "index"));
+    $router->add("/", array("controller" => "home"      , "action" => "index"));
+    $router->add("/{controller}/{action}");
 
 $params = $router->match($path);
 
