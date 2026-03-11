e63ca39ab27934327f996a65c8a9b02886fd4265
Lucas Marion <lucasmarion.dev@gmail.com>
2026-03-11 15:24:23 -0300

    Aula 84

--- src/App/Models/Product.php
+++ src/App/Models/Product.php
@@ -3,14 +3,14 @@
 namespace App\Models;
 
 use PDO;
+use App\Database;
 
 class Product {
 
     public function getData() {
 
-        $dsn = "mysql:host=localhost;dbname=product_db;charset=utf8;port=3306";
-
-        $pdo = new PDO($dsn, "product_db_user", "secret", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $database = new Database;
+        $pdo = $database->getConnection();
 
         $stmt = $pdo->query("SELECT * FROM product");
 
