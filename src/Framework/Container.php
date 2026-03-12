a0ec17b827d959de48a4f61b96151f2d46e3264d
Lucas Marion <lucasmarion.dev@gmail.com>
2026-03-12 11:08:37 -0300

    Aula 91 e 92

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -4,6 +4,7 @@ namespace Framework;
 
 use ReflectionClass;
 use Closure;
+use ReflectionNamedType;
 
 class Container {
 
@@ -28,7 +29,24 @@ class Container {
 
         $dependencies = array();
         foreach ($constructor->getParameters() as $parameter) {
-            $type = (string) $parameter->getType();
+
+            $type = $parameter->getType();
+
+            if ($type === null) {
+                echo "Constructor parameter '{$parameter->getName()}' in the $class_name class has no type declaration";
+                die();
+            }
+
+            if ( ! ($type instanceof ReflectionNamedType) ) {
+                echo "Constructor parameter '{$parameter->getName()}' in the $class_name class is an invalid type: '$type'. Only single named types supported";
+                die();
+            }
+
+            if ($type->isBuiltIn() === true) {
+                echo "Unable to resolve constructor parameter '{$parameter->getName()}' of type '$type' of the $class_name class";
+                die();
+            }
+
             $dependencies[] = $this->get($type);
         }
         
