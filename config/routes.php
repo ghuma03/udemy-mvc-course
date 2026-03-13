8401aa3e8f7920ae4866f14bb7477abd2704eb21
Lucas Marion <lucasmarion.dev@gmail.com>
2026-03-13 10:51:11 -0300

    Aula 107

---
+++ config/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+$router = new Framework\Router;
+    $router->add("/admin/{controller}/{action}", array("namespace" => "Admin"));
+    $router->add("/{title}/{id:\d+}/{page:\d+}", array("controller" => "products", "action" => "showPage"));
+    $router->add("/product/{slug:[\w-]+}", array("controller" => "products", "action" => "show"));
+    $router->add("/{controller}/{id:\d+}/{action}");
+    $router->add("/home/index", array("controller" => "home"      , "action" => "index"));
+    $router->add("/products", array("controller" => "products"  , "action" => "index"));
+    $router->add("/", array("controller" => "home"      , "action" => "index"));
+    $router->add("/{controller}/{action}");
+
+return $router;
--- index.php
+++ index.php
@@ -15,15 +15,7 @@ if ($path === false) {
     throw new UnexpectedValueException("Malformed URL: '{$_SERVER["REQUEST_URI"]}'");
 }
 
-$router = new Framework\Router;
-    $router->add("/admin/{controller}/{action}", array("namespace" => "Admin"));
-    $router->add("/{title}/{id:\d+}/{page:\d+}", array("controller" => "products", "action" => "showPage"));
-    $router->add("/product/{slug:[\w-]+}", array("controller" => "products", "action" => "show"));
-    $router->add("/{controller}/{id:\d+}/{action}");
-    $router->add("/home/index", array("controller" => "home"      , "action" => "index"));
-    $router->add("/products", array("controller" => "products"  , "action" => "index"));
-    $router->add("/", array("controller" => "home"      , "action" => "index"));
-    $router->add("/{controller}/{action}");
+$router = require "config/routes.php";
 
 $container = new Framework\Container;
     $container->set(App\Database::class, function() {
