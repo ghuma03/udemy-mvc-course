43e474282d211c9e9b8049649e8badb0ff4a656a
Lucas Marion <lucasmarion.dev@gmail.com>
2026-03-13 10:47:34 -0300

    Aula 106

--- index.php
+++ index.php
@@ -7,33 +7,7 @@ spl_autoload_register(function($class_name) {
 });
 
 set_error_handler("Framework\ErrorHandler::handleError");
-
-set_exception_handler(function(Throwable $exception) {
-
-    if ($exception instanceof Framework\Exceptions\PageNotFoundException) {
-        http_response_code(404);
-        $template = "404.php";
-    }
-    else {
-        http_response_code(500);
-        $template = "500.php";
-    }
-
-    $show_errors = true;
-
-    if ($show_errors === true) {
-        ini_set("display_errors", "1");
-    }
-    else {
-
-        ini_set("display_errors", "0");
-        ini_set("log_errors"    , "1");
-
-        require "views/$template";
-    }
-
-    throw $exception;
-});
+set_exception_handler("Framework\ErrorHandler::handleException");
 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
