758cc88fb8cd77f66594d404ffe6655d53afe918
Lucas Marion <lucasmarion.dev@gmail.com>
2026-03-13 08:42:39 -0300

    Aula 102

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 declare(strict_types = 1);
 
+set_error_handler(function(int $errno, string $errstr, string $errfile, int $errline): bool {
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+});
+
 set_exception_handler(function(Throwable $exception) {
 
     $show_errors = true;
