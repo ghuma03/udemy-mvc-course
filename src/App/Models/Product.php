97299a67f8fcb38c65dd7f00646cd6efff67a3f5
Lucas Marion <lucasmarion.dev@gmail.com>
2026-03-14 14:16:50 -0300

    Aula 120

--- src/App/Models/Product.php
+++ src/App/Models/Product.php
@@ -4,37 +4,8 @@ declare(strict_types = 1);
 
 namespace App\Models;
 
-use PDO;
-use App\Database;
+use Framework\Model;
 
-class Product {
+class Product extends Model {
 
-    private Database $database;
-
-    public function __construct(Database $database) {
-        $this->database = $database;
-    }
-
-    public function getData() {
-
-        $pdo = $this->database->getConnection();
-
-        $stmt = $pdo->query("SELECT * FROM product");
-
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    public function find(string $id): array|bool {
-
-        $conn = $this->database->getConnection();
-
-        $sql = "SELECT * FROM product WHERE id = :id";
-
-        $stmt = $conn->prepare($sql);
-            $stmt->bindValue(":id", $id, PDO::PARAM_INT);
-            $stmt->execute();
-        
-        return $stmt->fetch(PDO::FETCH_ASSOC);
-
-    }
 }
---
+++ src/Framework/Model.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Framework;
+
+use PDO;
+use App\Database;
+
+abstract class Model {
+
+    private Database $database;
+
+    public function __construct(Database $database) {
+        $this->database = $database;
+    }
+
+    public function getData() {
+
+        $pdo = $this->database->getConnection();
+
+        $stmt = $pdo->query("SELECT * FROM product");
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function find(string $id): array|bool {
+
+        $conn = $this->database->getConnection();
+
+        $sql = "SELECT * FROM product WHERE id = :id";
+
+        $stmt = $conn->prepare($sql);
+            $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+            $stmt->execute();
+        
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+
+    }
+}
