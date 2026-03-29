549255672d7def0763bc938a35f0cac9c34e7bf3
Lucas Marion <lucasmarion.dev@gmail.com>
2026-03-29 14:39:08 -0300

    Aula 167

--- src/Framework/MVCTemplateViewer.php
+++ src/Framework/MVCTemplateViewer.php
@@ -21,6 +21,7 @@ class MVCTemplateViewer implements TemplateViewerInterface {
             $code = $this->replaceYields($base, $blocks);
         }
 
+        $code = $this->loadIncludes($views_dir, $code);
         $code = $this->replaceVariables($code);
         $code = $this->replacePHP($code);
             
@@ -63,6 +64,19 @@ class MVCTemplateViewer implements TemplateViewerInterface {
 
             $code = preg_replace("#{% yield $name %}#", $block, $code);
 
+        }
+
+        return $code;
+    }
+
+    private function loadIncludes(string $dir, string $code): string {
+
+        preg_match_all('#{% include "(?<template>.*?)" %}#', $code, $matches, PREG_SET_ORDER);
+
+        foreach ($matches as $match) {
+            $template = $match["template"];
+            $contents = file_get_contents($dir . $template);
+            $code = preg_replace("#{% include \"$template\" %}#", $contents, $code);
         }
 
         return $code;
